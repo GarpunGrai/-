09e63020e7d92f761cf78756b551f7c2c4b1fd8f
GarpunGrai <110448515+GarpunGrai@users.noreply.github.com>
2022-08-02 12:38:46 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+os.system('start https://github.com/GarpunGrai')
+
+time.sleep(5)
+
+os.startfile('D:\Joxi\joxi.exe')
+
+time.sleep(7)
+
+os.startfile('C:\Program Files\Курс.txt')
+
+
